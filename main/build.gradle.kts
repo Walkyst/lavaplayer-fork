e789542a57b2660213a57d214d52453055878f19
giftshower <giftshower@giftshower.org>
2023-12-26 00:09:03 +0900

    Bump dependency and fix typo

--- main/build.gradle.kts
+++ main/build.gradle.kts
@@ -24,8 +24,8 @@ dependencies {
   implementation("net.iharder:base64:2.3.9")
   implementation("org.json:json:20231013")
 
-  testImplementation("org.codehaus.groovy:groovy:2.5.14")
-  testImplementation("org.spockframework:spock-core:2.4-M1")
+  testImplementation("org.apache.groovy:groovy-all:4.0.6")
+  testImplementation("org.spockframework:spock-core:2.4-M1-groovy-4.0")
   testImplementation("ch.qos.logback:logback-classic:1.4.12")
   testImplementation("com.sedmelluq:lavaplayer-test-samples:1.3.11")
 }
